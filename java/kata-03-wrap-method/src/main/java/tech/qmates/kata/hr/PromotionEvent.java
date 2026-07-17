ad5b64604160a92b680e08dd9edde6cef139e8d8
Daniele Megna <daniele.megna@qmates.tech>
2026-07-17 12:18:15 +0200

    Convert java PromotionEvent to record in kata-03-wrap-method

--- java/kata-03-wrap-method/src/main/java/tech/qmates/kata/hr/PromotionEvent.java
+++ java/kata-03-wrap-method/src/main/java/tech/qmates/kata/hr/PromotionEvent.java
@@ -3,33 +3,5 @@ package tech.qmates.kata.hr;
 /**
  * Domain event published when an employee is successfully promoted.
  */
-public class PromotionEvent {
-
-    private final String employeeId;
-    private final String newTitle;
-    private final boolean newLeadership;
-
-    public PromotionEvent(String employeeId, String newTitle, boolean newLeadership) {
-        this.employeeId = employeeId;
-        this.newTitle = newTitle;
-        this.newLeadership = newLeadership;
-    }
-
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public String getNewTitle() {
-        return newTitle;
-    }
-
-    public boolean isNewLeadership() {
-        return newLeadership;
-    }
-
-    @Override
-    public String toString() {
-        return "PromotionEvent{employeeId='" + employeeId + "', newTitle='" + newTitle
-                + "', newLeadership=" + newLeadership + "}";
-    }
+public record PromotionEvent(String employeeId, String newTitle, boolean newLeadership) {
 }
